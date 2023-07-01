59d9ba2b7b2cf983e2c0bed391093966691b492a
KlestovAlexej <klestov@hotmail.com>
2023-07-01 16:22:39 +0300

    Рабочий API-Key

--- Gateway/Examples.cs
+++ Gateway/Examples.cs
@@ -36,7 +36,7 @@ public class Examples
     /// <summary>
     /// Ключ API.
     /// </summary>
-    private static readonly string ApiKey = @"EN1-3600001:AmrWbKRHq1Bt7OPOo4Pu89i8AAmkyf0jW9rwajJpSXwdTJOxTvN58GKqST1dWyJe";
+    private static readonly string ApiKey = @"EN1-1:IwiSvMvin38JB1oShqsctnsOZTw67RNk30Hkqh1edsg5AXqWXwE6m9fWDndeC/Cx";
 
     /// <summary>
     /// Публичный корневой сертификат сервера для HTTPS.
